356d3c432e019e8cf539943819839a53638e5d54
Clara Kang <clara.kang91@gmail.com>
2017-05-02 16:30:04 -0700

    root path created

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 1234
 
+app.get('/', function(req, res) {
+  res.send('<h1>Welcome to Animaniacs!</h1>')
+})
 
 app.listen(PORT, function(){
   console.log("Server started on port:", PORT)
